7f618ebd284680a88883fba2a219da7ba1f910ce
gOdLike0731 <57154989+gOdLike0731@users.noreply.github.com>
2021-09-10 10:33:47 +0800

    Update save.php

--- save.php
+++ save.php
@@ -9,12 +9,12 @@
 	
 	$lname='none';
 	$contact1=strlen($contact);
-	$contact2=substr('$contact', 0, 2);
+	$contact2=substr($contact, 0, 2);
 
 	 
  	$age = date_diff(date_create($bday), date_create('now'))->y;
 
-	if($contact1==11 and $contact2==09){
+	if($contact1==11 and $contact2=='09'){
 		if(preg_match('/^[.a-zA-Z, ]*$/', $fname) ){
 
 		$sql = "INSERT INTO `mypropelrr` ( `firstname`, `lastname`, `gender`, `email`, `address`, `birth_date`, `age`) VALUES ('$fname', '$lname', '$gender', '$email', '$contact', '$bday','$age')";
